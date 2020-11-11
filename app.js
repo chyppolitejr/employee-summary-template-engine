14a285399de514215c262234d913d4eac41ece7e
chyppoli <claude.hyppolite@gmail.com>
2020-11-10 20:51:40 -0500

    add 3 arrays to hold different types of members. add code to continue adding members until user says they are done

--- app.js
+++ app.js
@@ -10,54 +10,85 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+let internArray = [];
+let engineerArray = [];
+let managerArray = [];
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-inquirer
-  .prompt([
-    {
-      type: "list",
-      name: "employeeType",
-      message: "What Type of Employee Would You Like To Enter?",
-      choices: ["Intern", "Engineer", "Manager"],
-    },
-    {
-      type: "input",
-      name: "employeeName",
-      message: "What is the Employee's Full Name",
-    },
-    {
-      type: "number",
-      name: "employeeId",
-      message: "What is the Employee's Id Number",
-    },
-    {
-      type: "input",
-      name: "employeeEmail",
-      message: "What is the Employee's Email Address?",
-    },
-    {
-      type: "input",
-      name: "employeeSchool",
-      message: "What School Does the Intern Attend?",
-      when: (answers) => answers.employeeType === "Intern",
-    },
-    {
-      type: "input",
-      name: "employeeOffice",
-      message: "What Office Number is this Manager Assigned to?",
-      when: (answers) => answers.employeeType === "Manager",
-    },
-    {
-      type: "input",
-      name: "employeeGithub",
-      message: "What is the Github name of the Engineer?",
-      when: (answers) => answers.employeeType === "Engineer",
-    },
-  ])
-  .then((response) => {
-    console.log(JSON.stringify(response));
-  });
+const promptUser = () =>
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "employeeType",
+        message: "What Type of Employee Would You Like To Enter?",
+        choices: ["Intern", "Engineer", "Manager"],
+      },
+      {
+        type: "input",
+        name: "employeeName",
+        message: "What is the Employee's Full Name",
+      },
+      {
+        type: "number",
+        name: "employeeId",
+        message: "What is the Employee's Id Number",
+      },
+      {
+        type: "input",
+        name: "employeeEmail",
+        message: "What is the Employee's Email Address?",
+      },
+      {
+        type: "input",
+        name: "employeeSchool",
+        message: "What School Does the Intern Attend?",
+        when: (answers) => answers.employeeType === "Intern",
+      },
+      {
+        type: "input",
+        name: "employeeOffice",
+        message: "What Office Number is this Manager Assigned to?",
+        when: (answers) => answers.employeeType === "Manager",
+      },
+      {
+        type: "input",
+        name: "employeeGithub",
+        message: "What is the Github name of the Engineer?",
+        when: (answers) => answers.employeeType === "Engineer",
+      },
+    ])
+    .then((answers) => {
+      addToArray(answers);
+      promptContinue().catch((err) => console.error(err));
+    });
+
+const promptContinue = () =>
+  inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "addMoreMembers",
+        message: "Would you like to add more team members?",
+      },
+    ])
+    .then((data) => {
+      if (data.addMoreMembers === true) {
+        promptUser();
+      } else {
+        console.log(data.addMoreMembers);
+        console.log("Your are done adding team members");
+        console.log(JSON.stringify(internArray));
+        console.log(JSON.stringify(engineerArray));
+        console.log(JSON.stringify(managerArray));
+      }
+    });
+
+//   .then((response) => {
+//     console.log(JSON.stringify(response));
+//   });
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -78,3 +109,15 @@ inquirer
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+function addToArray(answers) {
+  if (answers.employeeType === "Manager") {
+    managerArray.push(answers);
+  } else if (answers.employeeType === "Engineer") {
+    engineerArray.push(answers);
+  } else {
+    internArray.push(answers);
+  }
+}
+
+promptUser().catch((err) => console.error(err));
